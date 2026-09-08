318b39c369f941f2a890d87417405af2a38117a2
planillasugel04tse-code <planillasugel04tse@gmail.com>
2026-09-08 18:12:48 -0500

    Register surebet API router

--- src/betano_analyzer/main.py
+++ src/betano_analyzer/main.py
@@ -3,6 +3,7 @@ from datetime import date
 from fastapi import FastAPI
 
 from .api import router
+from .arbitrage_api import router as arbitrage_router
 from .db import initialize
 from .model_api import router as model_router
 from .oddspapi_api import router as oddspapi_router
@@ -14,6 +15,7 @@ initialize()
 
 app = FastAPI(title="Betano Live Analyzer", version="0.1.0")
 app.include_router(router)
+app.include_router(arbitrage_router)
 app.include_router(model_router)
 app.include_router(oddspapi_router)
 
