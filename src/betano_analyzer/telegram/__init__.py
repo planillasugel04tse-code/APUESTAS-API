c9b0fa4d998b597bb92dd12cc4e4d8c32679ce0f
planillasugel04tse-code <planillasugel04tse@gmail.com>
2026-09-10 12:08:37 -0500

    Export core Telegram collector

--- src/betano_analyzer/telegram/__init__.py
+++ src/betano_analyzer/telegram/__init__.py
@@ -1,6 +1,7 @@
 """Telegram is an input/source adapter for the core Betano Analyzer engines."""
 
+from .collector import TelegramCollector, start_telegram_listener
 from .parser import ParsedTelegramPick, parse_telegram_message
 from .service import process_telegram_signal
 
-__all__ = ["ParsedTelegramPick", "parse_telegram_message", "process_telegram_signal"]
+__all__ = ["TelegramCollector", "start_telegram_listener", "ParsedTelegramPick", "parse_telegram_message", "process_telegram_signal"]
