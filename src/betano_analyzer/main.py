d3511f112acc6c100958a62c791dc1de7383544f
planillasugel04tse-code <planillasugel04tse@gmail.com>
2026-09-14 16:42:44 -0500

    feat: register OddsPapi live odds router

--- src/betano_analyzer/main.py
+++ src/betano_analyzer/main.py
@@ -13,6 +13,7 @@ from .db import initialize
 from .health_api import router as health_router
 from .model_api import router as model_router
 from .oddspapi_api import router as oddspapi_router
+from .oddspapi_odds_api import router as oddspapi_odds_router
 from .oddspapi_scan_api import router as oddspapi_scan_router
 from .provider_accounts_api import router as provider_accounts_router
 from .provider_accounts_panel import provider_accounts_page
@@ -61,6 +62,7 @@ app.include_router(bookmakers_router)
 app.include_router(health_router)
 app.include_router(model_router)
 app.include_router(oddspapi_router)
+app.include_router(oddspapi_odds_router)
 app.include_router(oddspapi_scan_router)
 app.include_router(provider_accounts_router)
 app.include_router(range_strategy_router)
